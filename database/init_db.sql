c7e411c69e7a104f029a0e7f0f2fec07b6c916c7
Ebrahim Emad <ebemad8@gmail.com>
2026-04-11 15:17:19 +0200

    add db init

---
+++ database/init_db.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE airflow;
+
+CREATE DATABASE arsenalfc_analytics;
+
+CREATE USER analytics_user WITH PASSWORD 'analytics_pass';
+
+CREATE USER airflow WITH PASSWORD 'airflow';
+
+
+GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
+
+GRANT ALL PRIVILEGES ON DATABASE arsenalfc_analytics TO analytics_user;
+
+
+\c airflow
+
+GRANT ALL ON SCHEMA public TO airflow;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO airflow;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO airflow;
+
+ALTER SCHEMA public OWNER TO airflow;
+
+
+\c arsenalfc_analytics
+
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
+CREATE SCHEMA IF NOT EXISTS metrics;
+
+
+GRANT ALL ON SCHEMA bronze TO analytics_user;
+GRANT ALL ON SCHEMA silver TO analytics_user;
+GRANT ALL ON SCHEMA gold TO analytics_user;
+GRANT ALL ON SCHEMA metrics TO analytics_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA bronze GRANT ALL ON TABLES TO analytics_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA silver GRANT ALL ON TABLES TO analytics_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA gold GRANT ALL ON TABLES TO analytics_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA metrics GRANT ALL ON TABLES TO analytics_user;
